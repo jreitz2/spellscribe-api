dddb9b0ed8b5d1ecbce5b0bf3d57a1c637b8cc66
jreitz2 <jreitz2@gmail.com>
2023-08-03 14:06:38 -0400

    domain setting

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(session({
   cookie: {
     secure: true,
     httpOnly: true,
-    domain: "http://spellscribe-api.onrender.com",
+    domain: "https://spellscribe-api.onrender.com",
   },
  }));
 app.use(passport.initialize());
