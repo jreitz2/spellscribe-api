969a08dc478dce99149030607f6181a645f459db
jreitz2 <jreitz2@gmail.com>
2023-08-03 11:10:11 -0400

    deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 //middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://jreitz2.github.io/spellscribe/',
   credentials: true,
 }));
 app.use(express.json());
